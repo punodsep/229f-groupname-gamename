600117c4aa59864bb432af10e3f71cab30a692a6
pariyakorn saelao <pariyakorn.carrot28@gmail.com>
2026-03-24 18:47:57 +0700

    Add ShowTutorial () in LoadScene Script

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,13 +1,25 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
     public PauseMenu pauseMenu;
+    public GameObject tutorialPage;
 
     private void Start()
     {
-        pauseMenu.Resume();
+        if (pauseMenu != null)
+        {
+            pauseMenu.Resume();
+        }
+    }
+
+    public void ShowTutorial()
+    {
+        if (tutorialPage != null)
+        {
+            tutorialPage.SetActive(true); 
+        }
     }
 
     public void LoadSceneMain()
@@ -17,6 +29,6 @@ public class LoadScene : MonoBehaviour
 
     public void LoadSceneGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Game"); 
     }
 }
